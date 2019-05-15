6a4bf1be3a4885f334137317cff98396aed2c2bc
Sarkar, Anand (as0481) <as0481@att.com>
2019-05-15 16:50:48 -0400

    Sharable Error dialog
    Signed-off-by: Sarkar, Anand (as0481) <as0481@att.com>
    Issue-Id: RICPLT-1313
    Change-Id: Ib464c340545fc618321cca95838a37a7fcb2a44c
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

--- webapp-frontend/src/app/services/ui/error-dialog.service.ts
+++ webapp-frontend/src/app/services/ui/error-dialog.service.ts
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,28 +17,24 @@
  * limitations under the License.
  * ========================LICENSE_END===================================
  */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ErrorDialogComponent } from '../../ui/error-dialog/error-dialog.component';
+import { HttpErrorResponse } from '@angular/common/http';
+import { MatDialog } from '@angular/material/dialog';
+import { Injectable } from '@angular/core';
+ 
+@Injectable()
+export class ErrorDialogService {
 
-import { SignalComponent } from './signal.component';
+  public errorMessage: string = '';
+  constructor(private dialog: MatDialog) { }
+  public handleError(error: HttpErrorResponse){
 
-describe('SignalComponent', () => {
-  let component: SignalComponent;
-  let fixture: ComponentFixture<SignalComponent>;
+    return this.dialog.open(ErrorDialogComponent, {
+            width: '400px',
+            position: { top: '100px' },
+            disableClose: true,
+            data: {'errorMessage': error.message}
+        });
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SignalComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SignalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  }
+}
