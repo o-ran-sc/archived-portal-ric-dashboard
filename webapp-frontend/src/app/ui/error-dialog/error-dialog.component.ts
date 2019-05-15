156747534087caf0048bcf4305e72a011552496c
Sarkar, Anand (as0481) <as0481@att.com>
2019-05-15 16:50:48 -0400

    Sharable Error dialog
    Signed-off-by: Sarkar, Anand (as0481) <as0481@att.com>
    Issue-Id: RICPLT-1313
    Change-Id: Ib464c340545fc618321cca95838a37a7fcb2a44c
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

--- webapp-frontend/src/app/ui/error-dialog/error-dialog.component.ts
+++ webapp-frontend/src/app/ui/error-dialog/error-dialog.component.ts
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,28 +17,23 @@
  * limitations under the License.
  * ========================LICENSE_END===================================
  */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
-import { SignalComponent } from './signal.component';
+export interface ErrorData {
+  errorMessage: string;
+}
 
-describe('SignalComponent', () => {
-  let component: SignalComponent;
-  let fixture: ComponentFixture<SignalComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SignalComponent ]
-    })
-    .compileComponents();
-  }));
+@Component({
+  selector: 'app-error-dialog',
+  templateUrl: './error-dialog.component.html'
+})
+export class ErrorDialogComponent{
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SignalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  constructor(private dialogRef: MatDialogRef<ErrorDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: ErrorData) { }
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  public closeDialog = () => {
+    this.dialogRef.close();
+  }
+}
