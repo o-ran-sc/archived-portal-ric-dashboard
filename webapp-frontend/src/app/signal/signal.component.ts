9a010d3484eca7cbfd477818f99955af21a8cf56
Sarkar, Anand (as0481) <as0481@att.com>
2019-05-15 16:50:48 -0400

    Sharable Error dialog
    Signed-off-by: Sarkar, Anand (as0481) <as0481@att.com>
    Issue-Id: RICPLT-1313
    Change-Id: Ib464c340545fc618321cca95838a37a7fcb2a44c
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

--- webapp-frontend/src/app/signal/signal.component.ts
+++ webapp-frontend/src/app/signal/signal.component.ts
@@ -17,80 +17,12 @@
  * limitations under the License.
  * ========================LICENSE_END===================================
  */
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LocalDataSource } from 'ng2-smart-table';
-import { Router } from '@angular/router';
-import { MatDialog, MatDialogConfig, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material';
-import { MatRadioModule } from '@angular/material/radio';
-import { FormGroup, FormControl, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { MatDialog} from '@angular/material/dialog';
+import { AppRANConnectDialogComponent } from './signal.component.ranconnect-dialog';
 import { E2ManagerService } from '../services/e2-mgr/e2-mgr.service';
 import { E2SetupRequest } from '../interfaces/e2-mgr.types';
-
-@Component({
-  selector: 'app-signal-ranconnect-dialog',
-  templateUrl: 'signal.component.ranconnect-dialog.html',
-  styleUrls: ['signal.component.css']
-})
-
-export class RANConnectDialogComponent implements OnInit {
-
-  public ranDialogForm: FormGroup;
-
-  constructor(
-      public dialogRef: MatDialogRef<RANConnectDialogComponent>,
-      private service: E2ManagerService
-    ,
-      @Inject(MAT_DIALOG_DATA) public data: E2SetupRequest) {  }
-
-  ngOnInit() {
-    const namePattern = /^([A-Z]){4}([0-9]){6}$/;
-    const ipPattern = /((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/;
-    const portPattern = /^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$/;
-    this.ranDialogForm = new FormGroup({
-      ranType: new FormControl('endc'),
-      ranName: new FormControl('', [Validators.required, Validators.pattern(namePattern)]),
-      ranIp: new FormControl('', [Validators.required, Validators.pattern(ipPattern)]),
-      ranPort: new FormControl('', [Validators.required, Validators.pattern(portPattern)])
-    });
-  }
-
-  onCancel() {
-    this.dialogRef.close();
-  }
-
-  public setupConnection = (ranFormValue) => {
-    if (this.ranDialogForm.valid) {
-      this.executeSetupConnection(ranFormValue);
-    }
-  }
-
-  private executeSetupConnection = (ranFormValue) => {
-    const setupRequest: E2SetupRequest = {
-      ranName: ranFormValue.ranName,
-      ranIp:   ranFormValue.ranIp,
-      ranPort: ranFormValue.ranPort
-    };
-    if (ranFormValue.ranType === 'endc') {
-      this.service.endcSetup(setupRequest).subscribe((val: any[]) => {});
-    } else {
-      this.service.x2Setup(setupRequest).subscribe((val: any[]) => {});
-    }
-    this.dialogRef.close();
-  }
-
-  public hasError(controlName: string, errorName: string) {
-    if (this.ranDialogForm.controls[controlName].hasError(errorName)) { return true; }
-    return false;
-  }
-
-  public validateControl(controlName: string) {
-    if (this.ranDialogForm.controls[controlName].invalid && this.ranDialogForm.controls[controlName].touched) { return true; }
-    return false;
-  }
-
-} // class RANConnectDialogComponent
 
 @Component({
   selector: 'app-signal',
@@ -138,20 +70,20 @@ export class SignalComponent {
 
   source: LocalDataSource = new LocalDataSource();
 
-  constructor(private service: E2ManagerService
-  , public dialog: MatDialog, private http: HttpClient) {
-    this.service.getAll().subscribe((val: any[]) => this.source.load(val));
+  constructor(private service: E2ManagerService, public dialog: MatDialog) { }
+
+  onInit() {
+    this.service.getAll().subscribe((val: E2SetupRequest[]) => this.source.load(val));
   }
 
   openRanConnectDialog() {
-    const dialogRef = this.dialog.open(RANConnectDialogComponent, {
+    const dialogRef = this.dialog.open(AppRANConnectDialogComponent, {
       width: '450px',
-      data: { }
+      data: {}
     });
     dialogRef.afterClosed().subscribe(result => {
       this.service.getAll().subscribe((val: any[]) => this.source.load(val));
     });
   }
 
-}// class SignalComponent
-
+} // class SignalComponent
