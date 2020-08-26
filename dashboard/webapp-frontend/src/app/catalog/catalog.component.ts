2f59b161192d2af4ddba659c8b5b4a379e044cc1
Nicolas Hu <jh245g@att.com>
2020-08-26 15:18:50 -0400

    Extend Xapp deploy workflow
    Signed-off-by: Jun (Nicolas) Hu <jh245g@att.com>
    Issue-ID: OAM-109
    Change-Id: Ieeef49538102d6596b337942025208d0a92f94c3

--- dashboard/webapp-frontend/src/app/catalog/catalog.component.ts
+++ dashboard/webapp-frontend/src/app/catalog/catalog.component.ts
@@ -17,22 +17,21 @@
  * limitations under the License.
  * ========================LICENSE_END===================================
  */
-import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSort } from '@angular/material/sort';
 import { Subscription } from 'rxjs';
-import { finalize } from 'rxjs/operators';
-import { RicInstance } from '../interfaces/dashboard.types';
 import { XMXapp } from '../interfaces/app-mgr.types';
+import { RicInstance } from '../interfaces/dashboard.types';
 import { AppMgrService } from '../services/app-mgr/app-mgr.service';
 import { InstanceSelectorService } from '../services/instance-selector/instance-selector.service';
-import { LoadingDialogService } from '../services/ui/loading-dialog.service';
-import { UiService } from '../services/ui/ui.service';
-import { AppConfigurationComponent } from './../app-configuration/app-configuration.component';
 import { ConfirmDialogService } from '../services/ui/confirm-dialog.service';
-import { OnboardComponent } from './../onboard/onboard.component';
+import { LoadingDialogService } from '../services/ui/loading-dialog.service';
 import { NotificationService } from '../services/ui/notification.service';
+import { UiService } from '../services/ui/ui.service';
+import { DeployDialogComponent } from '../ui/deploy-dialog/deploy-dialog.component';
+import { AppConfigurationComponent } from './../app-configuration/app-configuration.component';
+import { OnboardComponent } from './../onboard/onboard.component';
 import { CatalogDataSource } from './catalog.datasource';
 
 @Component({
@@ -100,30 +99,24 @@ export class CatalogComponent implements OnInit, OnDestroy {
   }
 
   onDeployApp(app: XMXapp): void {
-    this.confirmDialogService.openConfirmDialog('Deploy application ' + app.name + '?')
-      .afterClosed().subscribe((res: boolean) => {
-        if (res) {
-          this.loadingDialogService.startLoading('Deploying ' + app.name);
-          this.appMgrService.deployXapp(this.instanceKey, app.name)
-            .pipe(
-              finalize(() => this.loadingDialogService.stopLoading())
-            )
-            .subscribe(
-              (response: HttpResponse<Object>) => {
-                this.notificationService.success('App deploy succeeded!');
-              },
-              ((her: HttpErrorResponse) => {
-                // the error field should have an ErrorTransport object
-                let msg = her.message;
-                if (her.error && her.error.message) {
-                  msg = her.error.message;
-                }
-                this.notificationService.warn('App deploy failed: ' + msg);
-              })
-            );
-        }
+    if (this.darkMode) {
+      this.panelClass = 'dark-theme';
+    } else {
+      this.panelClass = '';
+    }
+    const dialogRef = this.dialog.open(DeployDialogComponent, {
+      panelClass: this.panelClass,
+      width: '400px',
+      maxHeight: '1000px',
+      position: {
+        top: '10%'
+      },
+      data: {
+        xappName: app.name,
+        instanceKey: this.instanceKey
       }
-      );
+
+    });
   }
 
   onboard(): void {
